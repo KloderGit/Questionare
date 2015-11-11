a0837c59eb00a000a1d357a202a1c001fbf9e38f
KloderGit <kaligula3@gmail.com>
2015-11-11 13:28:34 +0300

    for switch1

--- QuestionareTest/Program.cs
+++ QuestionareTest/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using QuestionareEntityFramework;
 //using System.Data.Entity;
+using System.Diagnostics;
 
 namespace QuestionareTest
 {
@@ -14,9 +15,14 @@ namespace QuestionareTest
         {
             using (QustionareContex db = new QustionareContex())
             {
-
                 var users = db.Quests.Include("Answers");
+
+                db.Database.Log = (s => Debug.WriteLine(s));
+
                 Console.WriteLine("Список объектов:");
+
+                Debug.WriteLine("OOOOOOOOOOOOOO");
+
                 foreach (Quest u in users)
                 {
                     Console.WriteLine("{0}.{1} - {2}", u.Id, u.Text, u.Description);
