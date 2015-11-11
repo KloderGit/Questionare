6088582b067d102a494a829260a90c93a6184a44
KloderGit <kaligula3@gmail.com>
2015-11-11 10:47:50 +0300

    First commit new branch

--- QuestionareTest/Program.cs
+++ QuestionareTest/Program.cs
@@ -14,8 +14,7 @@ namespace QuestionareTest
         {
             using (QustionareContex db = new QustionareContex())
             {
-                // sdfsdfsdfsdf
-                //dfadsfasdfa 
+                // Первый комит новой ветки
                 var users = db.Quests.Include("Answers");
                 Console.WriteLine("Список объектов:");
                 foreach (Quest u in users)
